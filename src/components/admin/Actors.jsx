b6371e485e1431854d4c7f741a5ce8d5e750172c
ankittkamal <officiallyankit999@gmail.com>
2023-10-12 17:46:46 +0530

    Dashboard UI for Movie and Actors

--- src/components/admin/Actors.jsx
+++ src/components/admin/Actors.jsx
@@ -1,5 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { BsPencilSquare, BsTrash } from "react-icons/bs";
+import { getActors } from "../../api/actor";
 
 export default function Actors() {
-  return <div>Actors</div>;
+  const fetchActors = async () => {
+    const res = await getActors(0, 5);
+    console.log(res);
+  };
+  useEffect(() => {
+    fetchActors();
+  }, []);
+  return <div className="grid grid-cols-4 gap-3 my-5"></div>;
 }
+
+const ActorProfile = ({ profile }) => {
+  const [showOptions, setShowOptions] = useState(false);
+
+  const handleOnMouseEnter = () => {
+    setShowOptions(true);
+  };
+
+  const handleOnMouseLeave = () => {
+    setShowOptions(false);
+  };
+
+  const { name, about = "", avatar } = profile;
+
+  if (!profile) return null;
+
+  return (
+    <div className="bg-white shadow dark:shadow dark:bg-secondary rounded h-20 overflow-hidden">
+      <div
+        onMouseEnter={handleOnMouseEnter}
+        onMouseLeave={handleOnMouseLeave}
+        className="flex cursor-pointer relative"
+      >
+        <img
+          src={avatar}
+          alt={name}
+          className="w-20 aspect-square object-cover"
+        />
+
+        <div className="px-2">
+          <h1 className="text-xl text-primary dark:text-white font-semibold">
+            {name}
+          </h1>
+          <p className="text-primary dark:text-white">
+            {about.substring(0, 50)}
+          </p>
+        </div>
+        <Options visible={showOptions} />
+      </div>
+    </div>
+  );
+};
+
+const Options = ({ visible, onDeleteClick, onEditClick }) => {
+  if (!visible) return null;
+
+  return (
+    <div className="absolute inset-0 bg-primary bg-opacity-25 backdrop-blur-sm flex justify-center items-center space-x-5">
+      <button
+        onClick={onDeleteClick}
+        className="p-2 rounded-full bg-white text-primary hover:opacity-80 transition"
+        type="button"
+      >
+        <BsTrash />
+      </button>
+      <button
+        onClick={onEditClick}
+        className="p-2 rounded-full bg-white text-primary hover:opacity-80 transition"
+        type="button"
+      >
+        <BsPencilSquare />
+      </button>
+    </div>
+  );
+};
--- src/navigator/AdminNavigator.jsx
+++ src/navigator/AdminNavigator.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Actors from "../components/admin/Actors";
+import Dashboard from "../components/admin/Dashboard";
 import Header from "../components/admin/Header";
 import Movies from "../components/admin/Movies";
 import MovieUpload from "../components/admin/MovieUpload";
 import Navbar from "../components/admin/Navbar";
 import ActorUpload from "../components/models/ActorUpload";
 import NotFound from "../components/NotFound";
-import Dashboard from "../components/admin/Dashboard";
 
 export default function AdminNavigator() {
   const [showMovieUploadModal, setShowMovieUploadModal] = useState(false);
